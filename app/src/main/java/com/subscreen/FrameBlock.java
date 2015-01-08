28accc2f10210d21a427ab47c44f8bff19f01e0d
gerbias <gerbias@yahoo.com>
2015-01-07 21:31:10 -0800

    Initial commit of FrameBlock.

--- app/src/main/java/com/subscreen/FrameBlock.java
+++ app/src/main/java/com/subscreen/FrameBlock.java
@@ -1,11 +1,67 @@
 package com.subscreen;
 
+import java.util.Date;
+
 /**
  * Created by Nick on 1/1/2015.
  * Certain formats are based on the frame instead of the time; this cannot be determined reliably
  * Allow for the user to switch between different framerates dynamically, allowing for them to pick
  * the proper one once they have gotten out of sync.
  */
-/*public class FrameBlock implements TextBlock {
+public class FrameBlock implements TextBlock {
+    //24, 30, and 48 fps seem to be all the movie framerates that currently exist
+    double frameRates[] = {23.976, 29.97, 23.976*2};
+    double frameRateModifier = 0;
+    public String text;
+    public long startFrame;
+    public long endFrame;
+    public FrameBlock(String input, long s, long e)
+    {
+        startFrame = s;
+        endFrame = e;
+        text = input;
+        setFrameRate(0);
+    }
+    public void setFrameRate(int choice)
+    {
+        frameRateModifier = 1000.0/frameRates[choice];
+    }
+    public void firstDelay()
+    {
+        Date currentTime = new Date();
+        long toSleep = (long) Math.floor(startFrame*frameRateModifier) - (currentTime.getTime() - Main.rootTime);
+        if (toSleep <= 0)
+            return;
+        try {
+            Thread.sleep(toSleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void getText(Output _outputTo)
+    {
+        _outputTo.outputText(text);
+    }
+    public void secondDelay()
+    {
+        Date currentTime = new Date();
+        long toSleep = (long) Math.floor(endFrame*frameRateModifier) - (currentTime.getTime() - Main.rootTime);
+        if (toSleep <= 0)
+            return;
+        try {
+            Thread.sleep(toSleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public long getStartTime()
+    {
+        Date currentTime = new Date();
+        return (long) Math.floor(startFrame*frameRateModifier)-(currentTime.getTime() - Main.rootTime);
+    }
+    long convertFramerate(double newFPS)
+    {
+        Date currentTime = new Date();
+        return Math.round((currentTime.getTime()-Main.rootTime)*newFPS/1000);
+    }
 }
-*/
