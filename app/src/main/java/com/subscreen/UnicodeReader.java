a1e7f546422bb3c031eadbcaa98e9e1a2c40ad3f
gerbias <gerbias@yahoo.com>
2015-01-29 09:31:19 -0800

    Attempt at rewriting the Unicode reader to not read character by character; not currently working correctly.

--- app/src/main/java/com/subscreen/UnicodeReader.java
+++ app/src/main/java/com/subscreen/UnicodeReader.java
@@ -2,48 +2,64 @@ package com.subscreen;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-
+import java.nio.CharBuffer;
 
 public class UnicodeReader {
-
+    byte[] buffer = new byte[1024];
+    int bufferOffset = 0;
 	FileInputStream fis = null;
+    int limit = 0;
 	public UnicodeReader(String fileName) {
 		try {
 			fis = new FileInputStream(fileName);
+            limit = fis.read(buffer);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return;
 		}
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return;
+        }
 	}
 	public char[] readLine()
 	{
-		int tmpChar = -1;
-		char[] buffer = new char[1024];
 		char[] out = null;
-		int i = 0;
+		int i = bufferOffset;
+        int j = 0;
 		try {
-			while (fis.available() > 0)
-			{
-				tmpChar = fis.read();
-				if (tmpChar == '\n')
-				{
-					out = new char[i];
-					for (int j = 0; j < i; j++)
-					{
-						out[j] = buffer[j];
-					}
-					break;
-				}
-				buffer[i++] = (char) tmpChar;
-			}
+                out = new char[buffer.length];
+                while (true)
+                {
+                    if (i >= limit)
+                    {
+                        limit = fis.read(buffer);
+                        bufferOffset = 0;
+                        i = 0;
+                        out[j++] = (char) buffer[i++];
+                    }
+                    if (buffer[i] == '\r' || buffer[i] == '\n') {
+                        if ((i+1 < limit) && (buffer[i+1] == '\r' || buffer[i+1] == '\n'))
+                            i += 2;
+                        else
+                            i+= 1;
+                        out[j++] = 0;
+                        break;
+                    }
+                    out[j++] = (char) buffer[i++];
+                }
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        bufferOffset = i;
 		return out;
 	}
 	public void close()
@@ -58,6 +74,7 @@ public class UnicodeReader {
 	}
 	public int available() throws IOException
 	{
+        int test = fis.available();
 		return fis.available();
 	}
 }
