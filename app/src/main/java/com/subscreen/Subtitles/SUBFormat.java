cf3c0d62d47baba4930d9166c643ce8172c35ca5
gerbias <gerbias@yahoo.com>
2015-02-17 19:55:50 -0800

    Attempt to parse options provided; currently I don't have access to a wide variety of these, so I have implemented what I currently know of.

--- app/src/main/java/com/subscreen/Subtitles/SUBFormat.java
+++ app/src/main/java/com/subscreen/Subtitles/SUBFormat.java
@@ -35,6 +35,7 @@ public class SUBFormat implements SubtitleFormat {
         String buffer;
         Matcher m;
         String text;
+        String options;
         long startFrame, endFrame;
         try {
             while (in.available() > 0) {
@@ -45,6 +46,10 @@ public class SUBFormat implements SubtitleFormat {
                     startFrame = Integer.parseInt(m.group(1));
                     endFrame = Integer.parseInt(m.group(2));
                     text = m.group(3);
+                    if (text.charAt(0) == '{')
+                    {
+                        text = buildOptions(text.substring(1, text.indexOf('}')),text.substring(text.indexOf('}') + 1));
+                    }
                     for (int i = 0; i < replace.length; i++)
                         text = text.replace(replace[i],replaceWith[i]);
                     blocks.add(new FrameBlock(text,startFrame,endFrame,playerInstance));
@@ -55,5 +60,42 @@ public class SUBFormat implements SubtitleFormat {
         {
             e.printStackTrace();
         }
+    }
+    //This format allows for a variety of options that need to be parsed in order to be converted
+    //to proper HTML
+    String buildOptions(String input, String text)
+    {
+        StringBuilder startText = new StringBuilder("<font color=\"");
+        switch(input.charAt(0))
+        {
+            case 'y':
+            case 'Y':
+                startText.append("yellow\">");
+                break;
+            case 'r':
+            case 'R':
+                startText.append("red\">");
+                break;
+            case 'b':
+            case 'B':
+                startText.append("blue\">");
+                break;
+            default:
+                startText.append("white\">");
+                break;
+        }
+        switch (input.charAt(2))
+        {
+            case 'i':
+                startText.append("<i>");
+                break;
+            case 'b':
+                startText.append("<b>");
+                break;
+            case 'u':
+                startText.append("<u>");
+                break;
+        }
+        return startText.toString();
     }
 }
