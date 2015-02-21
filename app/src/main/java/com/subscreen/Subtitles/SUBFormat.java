0325af06c8e8fd7a790dacae55b366eb824edcf4
gerbias <gerbias@yahoo.com>
2015-02-21 13:34:15 -0800

    Implemented color format specifier.

--- app/src/main/java/com/subscreen/Subtitles/SUBFormat.java
+++ app/src/main/java/com/subscreen/Subtitles/SUBFormat.java
@@ -72,66 +72,91 @@ public class SUBFormat implements SubtitleFormat {
             return input;
         int i = 1;
         boolean doEndText = true;
-        String text = input.substring(input.indexOf('}')+1);
+        String text = input.substring(input.lastIndexOf('}')+1);
         StringBuilder startText = new StringBuilder("");
         StringBuilder endText = new StringBuilder("");
         boolean formatAllLines = false;
         boolean doOption = true;
-        switch(input.charAt(i))
-        {
-            //If we have a capital control code, then it means that we shouldn't end the font
-            //specification so that it carries onto the next line
-            case 'Y':
-                doEndText = false;
-            case 'y':
-            while(doOption) {
-                i += 2;
-                switch (input.charAt(i)) {
-                    case 'i':
-                        startText.append("<i>");
-                        endText.insert(0, "</i>");
-                        break;
-                    case 'b':
-                        startText.append("<b>");
-                        endText.insert(0, "</b>");
-                        break;
-                    case 'u':
-                        startText.append("<u>");
-                        endText.insert(0, "</u>");
-                        break;
-                    case 's':
-                        startText.append("<s>");
-                        endText.insert(0, "</s>");
-                        break;
-                }
-                if (input.length() <= i+1 || input.charAt(i+1) != ',')
-                    doOption = false;
-            }
-            break;
-            case 'P':
-                //Position would have no relation to how the text oriented on the device
-            case 'f':
-            case 'F':
-                //We don't care about font name since they might not be installed, so ignore it
-            case 's':
-            case 'S':
-                //Font size
-                //Let's ignore this since the font size on a monitor is much different from a small
-                //phone, and probably will not appear as intended
+        boolean doOptionBlock = true;
+        while (doOptionBlock) {
+            doEndText = true;
+            switch (input.charAt(i)) {
+                //If we have a capital control code, then it means that we shouldn't end the font
+                //specification so that it carries onto the next line
+                case 'Y':
+                    doEndText = false;
+                case 'y':
+                    while (doOption) {
+                        i += 2;
+                        switch (input.charAt(i)) {
+                            case 'i':
+                                startText.append("<i>");
+                                if (doEndText)
+                                    endText.insert(0, "</i>");
+                                break;
+                            case 'b':
+                                startText.append("<b>");
+                                if (doEndText)
+                                    endText.insert(0, "</b>");
+                                break;
+                            case 'u':
+                                startText.append("<u>");
+                                if (doEndText)
+                                    endText.insert(0, "</u>");
+                                break;
+                            case 's':
+                                startText.append("<s>");
+                                if (doEndText)
+                                    endText.insert(0, "</s>");
+                                break;
+                        }
+                        if (input.length() <= i + 1 || input.charAt(i + 1) != ',')
+                            if (input.charAt(i + 2) != '{')
+                                doOption = false;
+                            else
+                                i++;
+                    }
+                    break;
+                case 'P':
+                    //Position would have no relation to how the text oriented on the device
+                case 'f':
+                case 'F':
+                    //We don't care about font name since they might not be installed, so ignore it
+                case 's':
+                case 'S':
+                    //Font size
+                    //Let's ignore this since the font size on a monitor is much different from a small
+                    //phone, and probably will not appear as intended
                 /*i = 2;
                 while (input.charAt(i++) != '}');
                 startText.append("<font size=\"" + input.substring(2,i) +  "\">");
                 endText.append("</font>");*/
-                break;
-            case 'C':
-                doEndText = false;
-            case 'c':
-                //Text color
-                //MicroDVD format uses BBGGRR color format, so we need to switch around some of
-                //the values
-                char[] colors = input.substring(3,8).toCharArray();
-                endText.append("</font>");
-                break;
+                    break;
+                case 'C':
+                    doEndText = false;
+                case 'c':
+                    //Text color
+                    //MicroDVD format uses BBGGRR color format, so we need to switch around some of
+                    //the values
+                    char[] colors = input.substring(i+3, i+9).toCharArray();
+                    char tmp = colors[0];
+                    colors[0] = colors[5];
+                    colors[5] = tmp;
+                    tmp = colors[1];
+                    colors[1] = colors[4];
+                    colors[4] = tmp;
+                    startText.append("<font color=\"#");
+                    startText.append(colors);
+                    startText.append("\">");
+                    if (doEndText)
+                        endText.append("</font>");
+                    i+=10;
+                    if (input.charAt(i) != '{')
+                        doOptionBlock = false;
+                    break;
+                default:
+                    doOptionBlock = false;
+            }
         }
         startText.append(text);
         if (doEndText)
