4ef68dc392d15ab799cb9c83b666a4eedd6a3a15
gerbias <gerbias@yahoo.com>
2015-03-28 20:41:07 -0700

    SMI format should be closer to actually being supported properly.

--- app/src/main/java/com/subscreen/Subtitles/SMIFormat.java
+++ app/src/main/java/com/subscreen/Subtitles/SMIFormat.java
@@ -30,35 +30,70 @@ public class SMIFormat implements SubtitleFormat {
     }
 
     public void readLines(FileReaderHelper in, ArrayList<TextBlock> blocks) {
-        Pattern p = Pattern.compile("<SYNC Start=(\\d*) (?:End=(\\d*))*");
+        String replaceString[] = {"&nbsp;"};
+        String replaceWith[] = {" "};
+        String regexPattern = "<SYNC Start=(\\d*)(?:\\s)*(?:End=(\\d*))*>(.*)";
+        Pattern p = Pattern.compile(regexPattern,Pattern.CASE_INSENSITIVE);
         char[] buffer = null;
         String str = null;
         long startTime, endTime;
         TimeBlock prevBlock = null;
+        TimeBlock newBlock = null;
+        buffer = in.readLine();
+        str = new String(buffer);
         Matcher m;
         try {
             do {
+                String text = new String();
                 endTime = -1;
-                buffer = in.readLine();
-                str = new String(buffer);
                 m = p.matcher(str);
                 if (m.find()) {
                     startTime = Integer.parseInt(m.group(1));
-                    if (prevBlock != null) {
-                        prevBlock.endTime = startTime;
-                        blocks.add(prevBlock);
-                    }
-                    if (m.groupCount() > 1)
+                    if (m.group(2) != null)
                         endTime = Integer.parseInt(m.group(2));
-                    buffer = in.readLine();
-                    prevBlock = new TimeBlock(new String(buffer), startTime,playerInstance);
-                    //If we didn't find an end time, we need to use the start time of the next block
-                    //as the end time
-                    if (m.groupCount() > 1) {
-                        prevBlock.endTime = endTime;
-                        blocks.add(prevBlock);
-                        prevBlock = null;
+                    char[] origText = m.group(3).toCharArray();
+                    char[] newText = new char[origText.length];
+                    int j = 0;
+                    if (m.group(3).length() > 0)
+                    {
+                        for (int i = 0; i < origText.length; i++)
+                        {
+                            //If we find an opening tag, skip to the
+                            //closing tag
+                            if (origText[i] == '<')
+                                while (origText[++i] != '>');
+                            else
+                                newText[j++] = origText[i];
+                        }
+                        text = new String(newText);
                     }
+                    while (in.available() > 0) {
+                        str = new String(in.readLine());
+                        if (str.length() == 0)
+                            break;
+                        if (!str.startsWith("<SYNC"))
+                            text = text + str;
+                        else
+                            break;
+                    }
+                    for (int i = 0; i < replaceString.length; i++)
+                        text = text.replace(replaceString[0],replaceWith[i]);
+                    if (prevBlock != null && prevBlock.endTime == -1)
+                        prevBlock.endTime = startTime;
+                    //If we have an empty line of text, it will appear when using the prev/next
+                    //buttons to move through the text blocks. Thus we should not add any empty
+                    //lines, but instead use their starting time as the previous blocks ending time
+                    if (text.trim().length() > 0)
+                       newBlock = new TimeBlock(text, startTime,playerInstance);
+                    else
+                        continue;
+                    newBlock.endTime = endTime;
+                    blocks.add(newBlock);
+                    prevBlock = newBlock;
+                }
+                else
+                {
+                    str = new String(in.readLine());
                 }
             } while (in.available() > 0);
         }
@@ -67,7 +102,7 @@ public class SMIFormat implements SubtitleFormat {
             e.printStackTrace();
         }
         //If we didn't get an end time on the last block, set it to display for 5 seconds by default
-        if (prevBlock != null){
+        if (prevBlock != null && prevBlock.endTime == -1){
             prevBlock.endTime = prevBlock.startTime + 5*1000;
         }
     }
