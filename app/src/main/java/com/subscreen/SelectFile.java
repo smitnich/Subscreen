66d1fecfabaa2780b0345c9e54b5e13adcec25c4
gerbias <gerbias@yahoo.com>
2015-04-09 16:26:22 -0700

    Add a missing space to the external device not found error message.

--- app/src/main/java/com/subscreen/SelectFile.java
+++ app/src/main/java/com/subscreen/SelectFile.java
@@ -52,7 +52,7 @@ public class SelectFile extends FragmentActivity {
         try {
             isMounted = android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
             if (!isMounted) {
-                displayExitMessage("Sorry, we couldn't find the external device.\nMake sure it is"
+                displayExitMessage("Sorry, we couldn't find the external device.\nMake sure it is "
                 + "inserted and that your Android device is not in Mass Storage Mode.", "Storage not Found");
             }
             File subDirectory = new File(dirPath);
