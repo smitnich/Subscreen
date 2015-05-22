12a8e85c24ba5b08c136c881526431a77ca8c131
gerbias <smitnich@onid.orst.edu>
2015-05-22 11:07:20 -0700

    Make sure the subtitle file is closed after usage.

--- app/src/main/java/com/subscreen/SubtitlePlayer.java
+++ app/src/main/java/com/subscreen/SubtitlePlayer.java
@@ -2,6 +2,7 @@ package com.subscreen;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PushbackInputStream;
@@ -80,6 +81,13 @@ public class SubtitlePlayer {
             parentActivity.displayBackMessage(
                     context.getText(R.string.bad_format_message).toString(),"Sorry");
             return;
+        }
+        finally {
+            try {
+                fileData.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         if (smiSub != null) {
             languages = smiSub.getAvailableLanguages();
