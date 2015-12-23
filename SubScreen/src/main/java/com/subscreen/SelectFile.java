5e0657fa51683e4269adaaf116241d86bb49a18c
gerbias <smitnich@onid.orst.edu>
2015-12-22 16:04:34 -0800

    Allow the user to go back as far as the device root if they wish.

--- SubScreen/src/main/java/com/subscreen/SelectFile.java
+++ SubScreen/src/main/java/com/subscreen/SelectFile.java
@@ -163,7 +163,10 @@ public class SelectFile extends FragmentActivity {
     }
     private void goBackDirectory() {
         //Don't allow for going back past the subtitles directory
-        if (curPath.compareTo(dirPath) == 0) {
+        /*if (curPath.compareTo(dirPath) == 0) {
+            return;
+        }*/
+        if (curPath.compareTo("/") == 0) {
             return;
         }
         int i;
@@ -284,7 +287,8 @@ public class SelectFile extends FragmentActivity {
         if (!isMounted || !dir.isDirectory())
             return out;
         //If we're at the root directory, don't allow the user to go back
-        if (!path.equals(dirPath))
+        //if (!path.equals(dirPath))
+        if (!path.equals("/"))
             out.add(backString);
         for (File f : dir.listFiles(textFilter)) {
             if (f.isDirectory())
